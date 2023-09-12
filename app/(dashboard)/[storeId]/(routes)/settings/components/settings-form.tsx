49455f2708134aeedd5a1d94f840e8d615c3f5c4
Htivus <suvithkumar0001@gmail.com>
2023-09-12 20:10:14 +0530

    update store name

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -9,6 +9,11 @@ import { Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
 
 interface SettingsFormProps{
     initialData:Store;
@@ -26,13 +31,28 @@ export const SettingsForm:React.FC<SettingsFormProps> = ({
     initialData
 })=>{
 
+    const params=useParams();
+    const router=useRouter();
     const [open,setOpen]=useState(false);
     const [loading,setLoading]=useState(false);
 
-    const from = useForm<SettingsFormValues>({
+    const form = useForm<SettingsFormValues>({
         resolver:zodResolver(formSchema),
         defaultValues: initialData
     });
+
+    const onSumbit= async (data:SettingsFormValues)=>{
+        try {
+            setLoading(true);
+            await axios.patch(`/api/stores/${params.storeId}`,data);
+            router.refresh();
+            toast.success("Store updated.");
+        } catch (error) {
+            toast.error("Something went wrong");
+        }finally{
+            setLoading(false);
+        }
+    };
 
     return(
         <>
@@ -42,14 +62,39 @@ export const SettingsForm:React.FC<SettingsFormProps> = ({
             description="Manage store preferences "
         />
         <Button
+        disabled={loading}
         variant="destructive"
         size="sm"
-        onClick={()=>{}}
+        onClick={()=>setOpen(true)}
         >
             <Trash className="h-4 w-4"/>
         </Button>
     </div>
     <Separator/>
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSumbit)} className="space-y-8 w-full">
+        <div className="grid grid-cols-3 gap-8">
+            <FormField 
+            control={form.control}
+            name="name"
+            render={({field})=>(
+                <FormItem>
+                    <FormLabel>
+                        Name
+                    </FormLabel>
+                    <FormControl>
+                        <Input disabled={loading} placeholder="Store name"  {...field} />
+                    </FormControl>
+                    <FormMessage/>
+                </FormItem>
+            )}
+            />
+        </div>
+        <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+        </Button>
+        </form>
+    </Form>
     </>
     );
 };
